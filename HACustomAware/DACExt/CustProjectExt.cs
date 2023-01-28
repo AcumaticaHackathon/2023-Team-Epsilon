0c48d013454fee396442373900acfaaf932c0ac8
Stéphane Bélanger <sbelanger@asgardalliance.com>
2023-01-28 18:45:02 -0500

    Preparing GraphExt and DACExt

---
+++ HACustomAware/DACExt/CustProjectExt.cs
@@ -0,0 +1,12 @@
+﻿using PX.Data;
+using PX.SM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HA.Objects.Summit2023.Epsilon.CustomAware {
+    public class CustProjectExt : PXCacheExtension<CustProject> {
+    }
+}
